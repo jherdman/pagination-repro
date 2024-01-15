722aba0f3534205390eb86857782588eb3039147
James Herdman <jamesherdman@nulogy.com>
2024-01-15 15:23:44 -0500

    Repro

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Pagination } from "@nulogy/components";
+
+function App() {
+  return (
+    <>
+      <Pagination alignSelf="flex-end" />
+    </>
+  );
+}
+
+export default App;
